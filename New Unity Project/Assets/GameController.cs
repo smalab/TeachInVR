de0cdaf573e89fc7d6139a4ee81c638289ac7e56
oitsmalab <oitsmalab@gmail.com>
2014-11-12 15:28:21 +0900

    車と色

---
+++ New Unity Project/Assets/GameController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GameController : MonoBehaviour
+{
+	private List<GameObject> cameras = new List<GameObject> ();
+	private int current_camera_index = 0;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		var objects = GameObject.FindGameObjectsWithTag ("MainCamera");
+		foreach (GameObject game_object in objects)
+		{
+			if (objects != null)
+			{
+				cameras.Add (game_object);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Keypad5))
+		{
+			//参照カメラ切り替え
+			++current_camera_index;
+			if (cameras.Count <= current_camera_index)
+			{
+				current_camera_index = 0;
+			}
+			for (int i = 0; i < cameras.Count; ++i)
+			{
+				cameras[i].camera.enabled = i == current_camera_index;
+			}
+		}
+	}
+}
---
+++ New Unity Project/Assets/clickMove.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class clickMove : MonoBehaviour
+{
+	public Transform target;
+	
+	private void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			Vector3 vec = Input.mousePosition;
+			vec.z = 10f;
+			
+			target.position = camera.ScreenToWorldPoint(vec);
+		}
+	}
+}
