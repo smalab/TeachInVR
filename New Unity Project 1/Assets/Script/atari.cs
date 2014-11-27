81e1ea26865d0bde3c96633365a38d9cd71cc8af
oitsmalab <oitsmalab@gmail.com>
2014-11-27 18:25:28 +0900

    範囲で当たり判定ができるようになった

---
+++ New Unity Project 1/Assets/Script/atari.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class atari : MonoBehaviour {
+
+	// Use this for initialization
+	void OnControllerColliderHit(ControllerColliderHit hit ){
+		var obj = hit.gameObject;	 
+		Debug.Log(obj.name); 
+		if (obj.name == "cube2"){ 
+			Debug.Log("hit");
+
+		}
+
+	}
+
+}
