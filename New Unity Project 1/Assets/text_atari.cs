16bfbe320a9bea75bdce61fb55c271ce72552653
oitsmalab <oitsmalab@gmail.com>
2014-12-18 17:08:46 +0900

    text_atariを作成。現在未完成。

---
+++ New Unity Project 1/Assets/text_atari.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Reflection;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class text_atari : MonoBehaviour {
+	public GameObject Text;
+	private GameObject place_text;
+		place_text = GameObject.Find("Text");
+	public void OnCollisionStay(Collision collision){
+		if(hit.Gameobject=="Cube1"){
+		place_text.GetComponent<Text>().Text="sapporo"
+	}
+
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
