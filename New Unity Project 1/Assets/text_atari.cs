018037a905fb9c36ac1562cd91632d8ac8eb5c82
oitsmalab <oitsmalab@gmail.com>
2014-12-19 16:19:02 +0900

    text_atariの変更。現在エラーあり。

--- New Unity Project 1/Assets/text_atari.cs
+++ New Unity Project 1/Assets/text_atari.cs
@@ -6,17 +6,36 @@ using UnityEngine.UI;
 
 public class text_atari : MonoBehaviour {
 	public GameObject Text;
-	private GameObject place_text =  GameObject.Find("Text");
+	private GameObject PT =  GameObject.Find("place_text");
 
-	public void OnCollisionStay(Collision collision){
-		//if (collision.gameObject == "Cube1") {
-				//place_text.GetComponent<Text> ().text = "sapporo";
-		//}
+	public void OnCollisionEnter(Collision col){
+		if (col.gameObject.name == "Cube1") {
+				PT.GetComponent<Text> ().text = "sapporo";
+		}
 
-			GameObject gui = GameObject.Find("GUI Text");
-			if(gui != null) gui.guiText.text = "Hit !";
+			//GameObject gui = GameObject.Find("GUI Text");
+			//if(gui != null) gui.guiText.text = "Hit !";
 
 	}
+
+
+	static public T FindInParents<T>(GameObject go) where T : Component
+	{
+		if (go == null)return null;
+		var comp = go.Getcomponent<T>();
+
+		if (comp != null)
+			return comp;
+
+		Transform t = go.transform.parent;
+		while (t != null && comp == null)
+		{
+			comp = t.gameobject.Getcomponent<T>();
+			t = t.parent;
+		}
+		return comp;
+	}
+
 
 	// Use this for initialization
 	void Start () {
