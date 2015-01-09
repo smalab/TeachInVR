306e3a46b5adf3c0a55911c9190d068a7190659e
oitsmalab <oitsmalab@gmail.com>
2015-01-09 16:13:58 +0900

    文字の色や大きさが変更できるようになった。 reset_yukaオブジェクトを作成し表示した文字を削除することも成功した。

--- New Unity Project 1/Assets/Script/atari.cs
+++ New Unity Project 1/Assets/Script/atari.cs
@@ -8,19 +8,38 @@ public class atari : MonoBehaviour {
 	{
 		style = new GUIStyle();
 		style.normal.textColor = Color.red; 
-		style.fontSize = 30;
+		style.fontSize = 80;
 	}
 	// Use this for initialization
 	void OnCollisionEnter(Collision collision){
-		if(collision.gameObject.name == "sapporo_yuka"){
-			Debug.Log("sapporo111");
-			flag0=1;
+				if (collision.gameObject.name == "sapporo_yuka") {
+						Debug.Log ("sapporo111");
+						flag0 = 1;
+				}
+				if (collision.gameObject.name == "wakkanai_yuka") {
+						Debug.Log ("wakkanai111");
+						flag0 = 2;
+				}
+				if (collision.gameObject.name == "reset_yuka") {
+						flag0 = 0;
+				}
+				if (collision.gameObject.name == "tokachi") {
+						flag0 = 3;
+				}
 		}
-	}
 	void OnGUI () {
+		if (flag0 == 0) {
+			GUI.Label(new Rect(100,100,500,500), "",style);	
+		}
 		if (flag0 == 1){
 		// ラベルを表示する
-		GUI.Label(new Rect(50,50,500,500), "sapporo");
+			GUI.Label(new Rect(100,100,500,500), "札幌",style);
 	}
-}
+		if (flag0 == 2) {
+			GUI.Label(new Rect(100,100,500,500), "稚内",style);	
+		}
+		if (flag0 == 3) {
+			GUI.Label(new Rect(100,100,500,500), "十勝平野\nしてみた",style);	
+	}
+	}
 }
