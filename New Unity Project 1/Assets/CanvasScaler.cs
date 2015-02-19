36d31a186c683ab14885e88e2a45ccd10268424d
oitsmalab <oitsmalab@gmail.com>
2015-02-19 16:01:03 +0900

    トンネルを作ったことにより生じた地形のズレを修正。 ゲーム音声を2Dサウンドにすることで移動中も音がぶれることなく鳴るようになった

---
+++ New Unity Project 1/Assets/CanvasScaler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CanvasScaler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ New Unity Project 1/Assets/canvas.boo
@@ -0,0 +1,9 @@
+﻿import UnityEngine
+
+class canvas (MonoBehaviour): 
+
+	def Start ():
+		pass
+	
+	def Update ():
+		pass
